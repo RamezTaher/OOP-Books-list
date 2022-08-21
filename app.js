a59ab3cb77bd77e032b3c39e0c246789bd9ca1de
Ramez Taher <Rameztaher2002@gmail.com>
2022-08-21 15:36:51 +0100

    Object methods OOP

--- app.js
+++ app.js
@@ -1,36 +1,19 @@
-// Person Constructor
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName
-  this.lastName = lastName
+const personPrototypes = {
+  greeting: function () {
+    return `hello there ${this.firstName} ${this.lastName}`
+  },
 }
 
-// Greeting
-Person.prototype.greeting = function () {
-  return `Hello there ${this.firstName} ${this.lastName}`
-}
+const ramez = Object.create(personPrototypes)
+ramez.firstName = "Ramez"
+ramez.lastName = "Taher"
+ramez.age = 19
+console.log(ramez.greeting())
 
-const person1 = new Person("Ramez", "Taher")
+const mohamed = Object.create(personPrototypes, {
+  firstName: { value: "Mohamed" },
+  lastName: { value: "Traversy" },
+  age: { value: 20 },
+})
 
-// console.log(person1.greeting())
-
-// Customer Constructor
-function Customer(firstName, lastName, phone, isLogged) {
-  Person.call(this, firstName, lastName)
-  this.phone = phone
-  this.isLogged = isLogged
-}
-
-// Inherit the Person prototype methods
-Customer.prototype = Object.create(Person.prototype)
-
-// Make customer prototype return Customer
-Customer.prototype.constructor = Customer
-
-const customer1 = new Customer("foulen", "foulen", "5555555", "true")
-console.log(customer1.greeting())
-
-// Customer greeting
-Customer.prototype.greeting = function () {
-  return `Hello there ${this.firstName} ${this.lastName} welcome to our company`
-}
-console.log(customer1.greeting())
+console.log(mohamed.greeting())
